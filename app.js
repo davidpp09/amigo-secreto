65b95ddfe1a64fc7b19f79d9d00b06f0bb0d035f
David Pedraza <david9fran@gmail.com>
2025-01-28 22:54:49 -0600

    Función mostrar lista

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos=[];
-let contador=0;
-function agregarAmigo (){
-    let nombreDeAmigo = document.getElementById("amigo").value;
-    if(nombreDeAmigo == ""){
-        alert("Por favor, inserte un nombre.");
-    }else{
-        amigos.push(nombreDeAmigo);
-        document.querySelector("#amigo").value = "";
-    }
+let amigos = [];
+function agregarAmigo() {
+  let nombreDeAmigo = document.getElementById("amigo").value;
+  if (nombreDeAmigo == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(nombreDeAmigo);
+    document.querySelector("#amigo").value = "";
+  }
+  mostrarLista();
+}
+function mostrarLista() {
+  let elementoHTML = document.querySelector("#listaAmigos");
+  elementoHTML.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    var li = document.createElement("li");
+    li.innerText = amigos[i];
+    elementoHTML.appendChild(li);
+  }
 }
